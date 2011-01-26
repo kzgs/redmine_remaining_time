cf05517fd6a7d9cd2b852dd6f0280e53f7fb0ce4
Kazuyuki Ohgushi <kazuyuki.ohgushi@gmail.com>
2011-01-26 18:42:38 +0900

    created project

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_remaining_time do
+  name 'Redmine Remaining Time plugin'
+  author 'Kazuyuki Ohgushi'
+  description 'This is a plugin for managing remaining time'
+  version '0.0.1'
+  url 'https://github.com/kzgs/redmine_remaining_time'
+  author_url 'https://github.com/kzgs'
+end
