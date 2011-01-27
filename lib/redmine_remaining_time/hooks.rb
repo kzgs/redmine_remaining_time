f34d220527e741c6ba18ef228cfbea20438298d8
Kazuyuki Ohgushi <kazuyuki.ohgushi@gmail.com>
2011-01-28 02:09:00 +0900

    add remaining time into issue view

--- lib/redmine_remaining_time/hooks.rb
+++ lib/redmine_remaining_time/hooks.rb
@@ -1,7 +1,12 @@
+require 'pp'
+
 module RedmineRemainingTime
   class Hooks < Redmine::Hook::ViewListener
-    def view_issues_show_description_bottom(contest={})
-      return "remaining time: "
+    def view_issues_show_description_bottom(context={})
+      issue = context[:issue]
+      remaining_time = RemainingTime.find_by_issue_id(issue.id).try(:time) ||
+        issue.estimated_hours * (100-issue.done_ratio) / 100
+      return "remaining time: #{remaining_time}"
     end
   end
 end
